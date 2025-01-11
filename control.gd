bacb7dc9ae2a1556ed47893a35d79ad3a29c4cf6
Yang <lufeng199908@gmail.com>
2025-01-11 17:33:38 -0500

    udpate menu logic

--- control.gd
+++ control.gd
@@ -1,13 +1,42 @@
 extends Control
 
-# Reference to the AudioStreamPlayer
-@onready var beep_player = $AudioStreamPlayer
+var dial_sequence = []
+var target_sequence = [8, 8, 8, 6, 6, 0, 5, 8, 9, 0]
+var current_directory_path = "res://audio/"  # Start path
+
+@onready var audio_player = $AudioStreamPlayer
+
+
 
 func _ready():
-	for button in $GridContainer.get_children():
-		if button is Button:
-			button.connect("pressed", Callable(self, "_on_button_pressed").bind(button))
+	for i in range(10):  # Assuming buttons are named Button0 to Button9
+		var button = $GridContainer.get_node("Button" + str(i))
+		button.connect("pressed", self, "_on_button_pressed", [i])
 
-func _on_button_pressed(button):
-	print("Button pressed:", button.text)
-	beep_player.play()
+func _on_button_pressed(value):
+	dial_sequence.append(value)
+	check_dial_sequence()
+
+func check_dial_sequence():
+	if dial_sequence.size() > target_sequence.size():
+		dial_sequence.pop_front()  # Keep the sequence within the target size
+
+	if dial_sequence == target_sequence:
+		play_current_directory_audio()
+		print("Dial sequence correct. Entering the folder.")
+		# Reset dial_sequence if needed or handle it according to your logic
+
+func play_current_directory_audio():
+	var directory = Directory.new()
+	if directory.open(current_directory_path) == OK:
+		directory.list_dir_begin()
+		var filename = directory.get_next()
+		while filename != "":
+			if filename.ends_with(".mp3"):
+				audio_player.stream = load(current_directory_path + filename)
+				audio_player.play()
+				break
+			filename = directory.get_next()
+		directory.list_dir_end()
+	else:
+		print("Failed to open directory: ", current_directory_path)
--- grid_container.gd
+++ grid_container.gd
@@ -3,6 +3,9 @@ extends GridContainer
 @onready var audio_player = $AudioStreamPlayer  # Reference to the AudioStreamPlayer
 @onready var info_label = get_node('../Label')
 var sound_queue = []  # List to hold the queue of sounds
+var dial_sequence = []
+var target_sequence = [8, 8, 8, 6, 6, 0, 5, 8, 9, 0]
+var current_directory_path = "res://audio/voicebot1_main/"  # Start path
 
 
 func _ready():
@@ -13,16 +16,65 @@ func _ready():
 		if button is Button:
 			button.set_custom_minimum_size(Vector2(100, 100))
 			# Get the button's default font
+			
+	var root_path = "res://audio/voicebot1_main"
+	var folder_structure = read_directory_structure(root_path)
+	print(folder_structure)
 		
 	# Connect the finished signal of the AudioStreamPlayer
 	audio_player.connect("finished", Callable(self, "_on_audio_finished"))
 
 	for button in self.get_children():
 		if button is Button:
-			button.connect("pressed", Callable(self, "_on_button_pressed").bind(button))
+			button.connect("pressed", Callable(self, "_on_button_pressed").bind(button.text.to_int()))
 			#button.connect("pressed", "_on_button_pressed")
+			
+# Function to recursively read directory structure and build a nested dictionary
+func read_directory_structure(path):
+	var structure = {}
+	var directory = DirAccess.open(path)
+	
+	directory.list_dir_begin()
+	var filename = directory.get_next()
+	while filename != "":
+		if filename != "." and filename != ".." and not filename.ends_with(".import"):
+			if directory.current_is_dir():
+				structure[filename] = read_directory_structure(path + "/" + filename)
+			else:
+				structure[filename] = "file"  # Or use null or any other placeholder for files
+		filename = directory.get_next()
+	directory.list_dir_end()
+	return structure
 
+func _on_button_pressed(value):
+	dial_sequence.append(value)
+	check_dial_sequence()
 
+func check_dial_sequence():
+	if dial_sequence.size() > target_sequence.size():
+		dial_sequence.pop_front()  # Keep the sequence within the target size
+
+	if dial_sequence == target_sequence:
+		play_current_directory_audio()
+		print("Dial sequence correct. Entering the folder.")
+
+func play_current_directory_audio():
+	var directory = DirAccess.open(current_directory_path)
+	if directory:
+		directory.list_dir_begin()
+		var filename = directory.get_next()
+		while filename != "":
+			if filename.ends_with(".mp3") or filename.ends_with(".wav") :
+				audio_player.stream = load(current_directory_path + filename)
+				audio_player.play()
+				break
+			filename = directory.get_next()
+		directory.list_dir_end()
+	else:
+		print("Failed to open directory: ", current_directory_path)
+		
+		
+'''
 func _on_button_pressed(button: Button):
 	var button_text = button.text
 	# Play the default beep sound first
@@ -64,3 +116,4 @@ func play_voice(audio_path: String):
 		audio_player.play()
 	else:
 		print("Failed to load sound:", audio_path)
+'''
